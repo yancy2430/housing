7de834c314306045096ceb5b645faa626ed8bfc1
yangzhe <yancy@tdeado.com>
2020-03-23 23:21:47 +0800

    a

--- pages/message/message.js
+++ pages/message/message.js
@@ -46,18 +46,20 @@ Page({
 
       console.log("链接成功")
       wx.sendSocketMessage({
-        data: '{"key":"client_bind","data":{"appVersion":"1.0.0","osVersion":"80.0.3987.132","channel":"browser","packageName":"com.farsunset.cim","device":"Chrome","deviceId":"8943c89645724ecc8099812e0695fd84","account":"' + wx.getStorageSync('user').userInfo.id + '"},"timestamp":1584867292991}',
+        data: '{"key":"bind","data":{"appVersion":"1.0.0","osVersion":"80.0.3987.132","channel":"browser","packageName":"com.farsunset.cim","device":"Chrome","deviceId":"8943c89645724ecc8099812e0695fd84"},"timestamp":1584867292991,"token":"' + wx.getStorageSync('user').token + '"}',
       })
 
     });
     wx.onSocketMessage(function callback(res) {
       let data = JSON.parse(res.data);
-      if(data.receiver){
-        that.data.messages.push(data)
+      console.log(data)
+      if (data.key == 'message'){
+        that.data.messages.push(data.data)
         that.setData({
           messages:that.data.messages
         })
       }
+      console.log(that.data.messages)
     });
 
     wx.onSocketError(function (err) {
@@ -73,11 +75,9 @@ Page({
     console.log(e.detail)
 
     let data={
-      sender: wx.getStorageSync('user').userInfo.id,
       receiver: wx.getStorageSync('user').sourceId,
       content: e.detail.value,
-      action: "0",
-      timestamp: new Date().getTime()
+      contentType: "0"
     }
 
     this.data.messages.push(data)
